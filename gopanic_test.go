641678f7e02ada5e7f6576890b839cc481ad42f1
VonC <vonc@laposte.net>
2014-12-18 16:36:45 +0100

    Read test file

--- gopanic_test.go
+++ gopanic_test.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
+	. "github.com/VonC/godbg"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGoPanic(t *testing.T) {
 	Convey("Test main", t, func() {
 		fmt.Println("test main")
+		files, err := ioutil.ReadDir("./tests")
+		if err != nil {
+			Pdbgf("Unable to access tests folder\n'%v'\n", err)
+			t.Fail()
+		}
+		for _, file := range files {
+			fmt.Println(file.Name())
+		}
 	})
 }
