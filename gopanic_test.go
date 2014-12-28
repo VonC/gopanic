2f4f31d7c7245bed34116c452d2785aa23dcdb1f
VonC <vonc@laposte.net>
2014-12-28 21:29:52 +0100

    Focus on test1

--- gopanic_test.go
+++ gopanic_test.go
@@ -19,7 +19,7 @@ func TestGoPanic(t *testing.T) {
 			t.Fail()
 		}
 		for _, file := range files {
-			if file.Name() != "exceptionstack2" {
+			if file.Name() != "exceptionstack1" {
 				continue
 			}
 			Pdbgf(file.Name())
