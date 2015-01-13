1166bcf0381c440f9483727f511d553856dacfed
VonC <vonc@laposte.net>
2015-01-13 10:42:31 +0000

    Replace VonC by current username for test portability

--- gopanic_test.go
+++ gopanic_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
+	"os/user"
+	"regexp"
+	"strings"
 	"testing"
 
 	. "github.com/VonC/godbg"
@@ -23,10 +25,23 @@ func TestGoPanic(t *testing.T) {
 				continue
 			}
 			Pdbgf(file.Name())
-			if in, err = os.Open("tests/" + file.Name()); err != nil {
+			user, err := user.Current()
+			if err != nil {
+				Pdbgf("Unable to access current user\n'%v'\n", err)
+				t.Fail()
+			}
+			username := user.Username
+			re := regexp.MustCompile(`^.*\\`)
+			username = re.ReplaceAllLiteralString(username, "")
+			var inb []byte
+			if inb, err = ioutil.ReadFile("tests/" + file.Name()); err != nil {
 				Pdbgf("Unable to access open file '%v'\n'%v'\n", file.Name(), err)
 				t.Fail()
 			}
+			ins := string(inb)
+			ins = strings.Replace(ins, "C:/Users/vonc/", "C:/Users/"+username+"/", -1)
+			in = strings.NewReader(ins)
+			// Pdbgf("In '%s'", ins)
 			var resb []byte
 			if resb, err = ioutil.ReadFile("tests/" + file.Name() + ".res"); err != nil {
 				Pdbgf("Unable to access open RES file '%v'\n'%v'\n", file.Name()+".res", err)
